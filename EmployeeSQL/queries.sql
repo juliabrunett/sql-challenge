b93ca5e58bef59a1fb6d529e189fa663452897ab
Julia Brunett <julia.h.brunett@gmail.com>
2021-05-05 11:49:47 -0700

    Worked on queries

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -40,3 +40,19 @@ e.emp_no = de.emp_no
 INNER JOIN departments AS d ON
 de.dept_no = d.dept_no
 WHERE dept_name = 'Sales';
+
+-- Query 7: Sales and Development Department Employees
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS de ON
+e.emp_no = de.emp_no
+INNER JOIN departments AS d ON
+de.dept_no = d.dept_no
+WHERE dept_name = 'Sales'
+OR dept_name = 'Development';
+
+-- Query 8: Frequency of Employee Last Names
+SELECT e.last_name, COUNT(e.last_name) AS Frequency
+FROM employees as e
+GROUP BY e.last_name
+ORDER BY Frequency DESC;
