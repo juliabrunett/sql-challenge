ec6cd53be788b2fa12935117cb48645a5c32d791
Julia Brunett <julia.h.brunett@gmail.com>
2021-05-05 11:31:00 -0700

    Worked on more queries

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -8,3 +8,13 @@ e.emp_no = s.emp_no
 SELECT e.first_name, e.last_name, e.hire_date
 FROM employees AS e
 WHERE EXTRACT(YEAR FROM hire_date) = '1986';
+
+-- Query 3
+SELECT m.dept_no, d.dept_name, m.emp_no, e.last_name, e.first_name
+FROM employees AS e
+INNER JOIN dept_manager as m ON
+e.emp_no = m.emp_no
+INNER JOIN departments as d ON
+m.dept_no = d.dept_no;
+
+
