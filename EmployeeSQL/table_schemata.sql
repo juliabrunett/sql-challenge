667491422e01f912387abdcf10358d05dbbb09bc
Julia Brunett <julia.h.brunett@gmail.com>
2021-05-06 12:35:02 -0700

    Updated table schemata code

--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -1,3 +1,10 @@
+DROP TABLE salaries;
+DROP TABLE dept_emp;
+DROP TABLE dept_manager;
+DROP TABLE departments;
+DROP TABLE employees;
+DROP TABLE titles;
+
 -- CREATING TABLES TO IMPORT CSV FILES INTO
 -- Create titles table
 CREATE TABLE titles (
@@ -31,14 +38,15 @@ CREATE TABLE departments (
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(5) NOT NULL,
-	PRIMARY KEY (emp_no, dept_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 -- Create department managers table
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(5) NOT NULL,
 	emp_no INT NOT NULL,
-	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
