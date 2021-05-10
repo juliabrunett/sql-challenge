0a1dcfa20fcf2d4338d0edd865cb1c3726100ba6
Julia Brunett <julia.h.brunett@gmail.com>
2021-05-10 11:47:46 -0700

    Updated tableschemata code

--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -1,3 +1,4 @@
+-- Reset Tables
 DROP TABLE salaries;
 DROP TABLE dept_emp;
 DROP TABLE dept_manager;
@@ -16,10 +17,10 @@ CREATE TABLE titles (
 -- Create employees table
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
-	emp_title_id VARCHAR(10) NOT NULL,
+	emp_title_id VARCHAR(5) NOT NULL,
 	birth_date DATE NOT NULL,
-	first_name VARCHAR(20) NOT NULL,
-	last_name VARCHAR(20) NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
 	sex VARCHAR(1) NOT NULL,
 	hire_date DATE NOT NULL,
 	PRIMARY KEY (emp_no),
@@ -34,27 +35,30 @@ CREATE TABLE departments (
 );
 
 
--- Create department employees table
+-- Create department employees table (similar to junction)
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(5) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
--- Create department managers table
+-- Create department managers table (similar to junction)
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(5) NOT NULL,
 	emp_no INT NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no)
 );
 
 -- Create salaries table
 CREATE TABLE salaries (
 	emp_no INT NOT NULL,
 	salary INT NOT NULL,
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 -- Output to check proper import of CSV files
